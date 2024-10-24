7782f1f7cc37139b9e9519daa5b8d416a142592c
Furkan Erdoğuş <107814668+PLADSS@users.noreply.github.com>
2024-10-24 14:33:04 +0300

    02_hotfix
    02_hotfix

--- tiktok/babel.config.js
+++ tiktok/babel.config.js
@@ -1,12 +1,9 @@
-const { default: plugin } = require("tailwindcss");
-
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    // presets: ['babel-preset-expo'],
-    // plugins: ["nativewind/babel"],
-
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['module:react-native-dotenv'],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['nativewind/babel']
+  ],
 };
